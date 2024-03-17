29c26371a9d32d252064311095ba316fd5bc6221
ldw0123 <ldw0123@gmail.com>
2024-03-18 01:24:55 +0900

    study: class

--- 03-TypeScript/02_class.ts
+++ 03-TypeScript/02_class.ts
@@ -4,7 +4,7 @@
 // constructor() 메서드. 매개변수를 가질 수 있다
 // 객체의 초기화를 담당. class로부터 객체를 생성할 때 호출된다
 
-// 접근 제한자
+// 접근 제어자
 // public - 클래스의 외부에서 접근 가능 (default)
 // private - 클래스 내부에서만 접근 가능
 // protected - 클래스 내부 + 상속받은 자식 클래스에서 접근 가능
@@ -22,5 +22,6 @@ abstract class User {
 class People extends User {}
 
 const oreo = new People('nico', 'las', '니꼬');
+
 console.log(oreo.nickname); // 니꼬
 console.log(oreo.getFullName()); // nico las
