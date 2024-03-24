45fd18dc99b4924096c804cc7803a2b32f01fba5
ldw0123 <ldw0123@gmail.com>
2024-03-25 05:05:18 +0900

    ✨ module 생성

--- blockchain-project/src/index.ts
+++ blockchain-project/src/index.ts
@@ -1,8 +1,11 @@
 // Block Chain Project
 
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return 'hi';
-  }
-}
+import { init, exit } from 'myPackage';
+
+init({
+  url: 'true',
+});
+
+exit(1);
+
+localStorage.clear();
