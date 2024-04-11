f3a641f752d5d91b1c9230b234a9390d0abdc0f7
ldw0123 <ldw0123@gmail.com>
2024-04-11 18:08:36 +0900

    404 not found 처리

---
+++ 25-Next_js/02-foodies/app/not-found.js
@@ -0,0 +1,9 @@
+// 404 error 처리 페이지
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>404 Not Found</h1>
+      <p>안타깝게도 요청하신 페이지를 찾을 수 없습니다</p>
+    </main>
+  );
+}
