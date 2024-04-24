5e5ec62c47335ff7d1e89d47f7ab4bc0cf77c9e7
ldw0123 <ldw0123@gmail.com>
2024-04-24 23:52:54 +0900

    ✨ Meetup 양식 추가

--- 25-Next_js/04-advanced-project/pages/new-meetup/index.js
+++ 25-Next_js/04-advanced-project/pages/new-meetup/index.js
@@ -1,1 +1,9 @@
 // localhost:3000/new-meetup
+import NewMeetupForm from '@/components/meetups/NewMeetupForm';
+
+export default function NewMeetupPage() {
+  function addMeetupHandler() {}
+
+  // props로 실행 결과가 아니라, 포인터를 넣어주기 때문에 함수에 괄호가 없다
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
