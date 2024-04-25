836954c9e3b62794808bfb7f5546fb1cae239666
ldw0123 <ldw0123@gmail.com>
2024-04-25 23:56:59 +0900

    ✨ _app.js 파일로 레이아웃을 전역으로 관리

--- 25-Next_js/04-advanced-project/pages/_app.js
+++ 25-Next_js/04-advanced-project/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+// _app.js : 최상위 컴포넌트 역할
+
+import Layout from '@/components/layout/Layout';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
