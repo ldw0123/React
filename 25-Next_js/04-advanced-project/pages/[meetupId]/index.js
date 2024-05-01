e91dfbe60839412559be833dc74c930b0fbf9e44
ldw0123 <ldw0123@gmail.com>
2024-05-01 23:58:49 +0900

    ✨ params 작업

--- 25-Next_js/04-advanced-project/pages/[meetupId]/index.js
+++ 25-Next_js/04-advanced-project/pages/[meetupId]/index.js
@@ -12,3 +12,21 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticProps(context) {
+  // single meetup에 데이터를 가져오기(fetch)
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRciG1cJNU2FQOPpOBcaAw9NCxixEPfW_YQSXHhfVOkitN0ZFQ1_Bf_vcWepNDleYklz0zA7jbfQ6FlK5NDQ6v5MvP8cDUN28F8UX0rtw',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Seoul City',
+        description: 'The first meetup',
+      },
+    },
+  };
+}
