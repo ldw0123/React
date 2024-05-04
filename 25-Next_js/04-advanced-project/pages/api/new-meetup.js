8e3d536420ad72ae5b453a7f5f2362549367a9a9
ldw0123 <ldw0123@gmail.com>
2024-05-04 23:54:33 +0900

    ✨ mongoDB

--- 25-Next_js/04-advanced-project/pages/api/new-meetup.js
+++ 25-Next_js/04-advanced-project/pages/api/new-meetup.js
@@ -2,11 +2,18 @@
 // URL: /api/new-meetup
 // POST /api/new-meetup
 
+import { MongoClient } from 'mongodb';
+
 export default function handler(req, res) {
   // POST 요청일 때만 함수를 작동시킴
   if (req.method === 'POST') {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    // MongoDB cluster 연결
+    MongoClient.connect(
+      'mongodb+srv://ldw0123:<password>@cluster0.fc5z6ma.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+    );
   }
 }
