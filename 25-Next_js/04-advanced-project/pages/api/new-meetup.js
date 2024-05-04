f10aea12445627ab751e66ad70c91fced2355f40
ldw0123 <ldw0123@gmail.com>
2024-05-05 00:47:58 +0900

    ✨ MongoDB

--- 25-Next_js/04-advanced-project/pages/api/new-meetup.js
+++ 25-Next_js/04-advanced-project/pages/api/new-meetup.js
@@ -4,16 +4,27 @@
 
 import { MongoClient } from 'mongodb';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   // POST 요청일 때만 함수를 작동시킴
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
-
-    // MongoDB cluster 연결
-    MongoClient.connect(
+    // MongoDB cluster   연결
+    const client = await MongoClient.connect(
       'mongodb+srv://ldw0123:<password>@cluster0.fc5z6ma.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
     );
+
+    const db = client.db(); // meetups에 연결 중인 데이터베이스를 확보
+
+    const meetupsCollection = db.collection('meetups'); // collection: SQL DB에 있는, 여러 문서를 보관하고 있는 테이블
+
+    const result = await meetupsCollection.insertOne(data); // collection에 새 문서를 삽입
+
+    console.log(result);
+
+    client.close();
+
+    // 201 상태 코드(성공) + JSON 데이터 추가
+    res.status(201).json({ massage: 'Meetup 삽입됨!' });
   }
 }
