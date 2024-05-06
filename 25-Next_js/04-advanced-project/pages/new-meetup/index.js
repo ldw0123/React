658dba6a286988b7e250f393b6887d7ab1b35503
ldw0123 <ldw0123@gmail.com>
2024-05-06 23:32:48 +0900

    ✨ API 경로로 HTTP request 요청 보내기

--- 25-Next_js/04-advanced-project/pages/new-meetup/index.js
+++ 25-Next_js/04-advanced-project/pages/new-meetup/index.js
@@ -1,8 +1,28 @@
 // localhost:3000/new-meetup
+import { useRouter } from 'next/router';
 import NewMeetupForm from '@/components/meetups/NewMeetupForm';
 
 export default function NewMeetupPage() {
-  function addMeetupHandler() {}
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupData) {
+    // API 경로로 request 보내기
+    // 인자 1: api/new-meetup.js 파일의 절대 경로로 생성
+    // 인자 2: request를 구성할 객체
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+
+    router.replace('/');
+  }
 
   // props로 실행 결과가 아니라, 포인터를 넣어주기 때문에 함수에 괄호가 없다
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
