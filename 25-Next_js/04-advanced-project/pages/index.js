bd22f7d493ccabdbf981d39a6451ae51f54fd916
ldw0123 <ldw0123@gmail.com>
2024-05-08 23:35:37 +0900

    ✨ MongoDB에 연결해서 데이터 가져오기

--- 25-Next_js/04-advanced-project/pages/index.js
+++ 25-Next_js/04-advanced-project/pages/index.js
@@ -1,25 +1,26 @@
 // 시작 페이지 localhost:3000/
 import MeetupList from '@/components/meetups/MeetupList';
+import { MongoClient } from 'mongodb';
 
 // 더미 데이터
-const DUMMY_MEETUPS = [
-  {
-    id: '1',
-    title: 'A First Meetup',
-    image:
-      'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRciG1cJNU2FQOPpOBcaAw9NCxixEPfW_YQSXHhfVOkitN0ZFQ1_Bf_vcWepNDleYklz0zA7jbfQ6FlK5NDQ6v5MvP8cDUN28F8UX0rtw',
-    address: 'some address 5, Seoul City',
-    description: 'This is a first meetup!',
-  },
-  {
-    id: '2',
-    title: 'A Second Meetup',
-    image:
-      'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQnY0F0cjQSVLuQfrHpu0DBpw4f999fpShw11XzBTl25pcaQllopm4KPw7Jtqde0rEybMBCKGXpZLiG4G5Q3kOAPJuzSilu_X86nnfFsA',
-    address: 'some address 10, Busan City',
-    description: 'This is a second meetup!',
-  },
-];
+// const DUMMY_MEETUPS = [
+//   {
+//     id: '1',
+//     title: 'A First Meetup',
+//     image:
+//       'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRciG1cJNU2FQOPpOBcaAw9NCxixEPfW_YQSXHhfVOkitN0ZFQ1_Bf_vcWepNDleYklz0zA7jbfQ6FlK5NDQ6v5MvP8cDUN28F8UX0rtw',
+//     address: 'some address 5, Seoul City',
+//     description: 'This is a first meetup!',
+//   },
+//   {
+//     id: '2',
+//     title: 'A Second Meetup',
+//     image:
+//       'https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQnY0F0cjQSVLuQfrHpu0DBpw4f999fpShw11XzBTl25pcaQllopm4KPw7Jtqde0rEybMBCKGXpZLiG4G5Q3kOAPJuzSilu_X86nnfFsA',
+//     address: 'some address 10, Busan City',
+//     description: 'This is a second meetup!',
+//   },
+// ];
 
 export default function HomePage(props) {
   /*
@@ -55,9 +56,29 @@ export async function getStaticProps() {
     이 함수에서 DUMMY_MEETUPS를 읽어들이고 준비한 다음, 이 페이지 컴포넌트에서 사용할 props로 설정된다. 따라서, 이 페이지 컴포넌트에서는 더 이상 상태를 관리할 필요가 없고, useEffect도 필요하지 않다 */
   /* revalidate 프로퍼티
     '수'초 마다 페이지를 새로 생성. 배포 후 서버에서 때때로 다시 사전 생성한다. 그러니 일부 데이터가 변경되었다고 해서 매번 다시 빌드하고 배포할 필요가 없음 */
+
+  // MongoDB에 연결
+  const client = await MongoClient.connect(
+    'mongodb+srv://ldw0123:<password>@cluster0.fc5z6ma.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+  );
+
+  const db = client.db(); // meetups에 연결 중인 데이터베이스를 확보
+
+  const meetupsCollection = db.collection('meetups'); // collection: SQL DB에 있는, 여러 문서를 보관하고 있는 테이블
+
+  const meetupsData = await meetupsCollection.find().toArray(); // 해당 컬렉션에 있는 모든 문서를 찾는다
+
+  client.close();
+
+  // MongoDB 가져온 데이터
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetupsData.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 10, // 10초 마다 페이지 다시 생성
   };
