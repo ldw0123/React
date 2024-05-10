4688472688d594d4c902adc7f9586a54a9e882a7
ldw0123 <ldw0123@gmail.com>
2024-05-10 23:32:40 +0900

    ✨ MongoDB에 연결하여 Meetup 세부 정보 데이터를 동적으로 불러오기

--- 25-Next_js/04-advanced-project/pages/[meetupId]/index.js
+++ 25-Next_js/04-advanced-project/pages/[meetupId]/index.js
@@ -1,34 +1,41 @@
 // 동적 페이지
 
 import MeetupDetail from '@/components/meetups/MeetupDetail';
+import { MongoClient, ObjectId } from 'mongodb';
 
-export default function MeetupDetails() {
+export default function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRciG1cJNU2FQOPpOBcaAw9NCxixEPfW_YQSXHhfVOkitN0ZFQ1_Bf_vcWepNDleYklz0zA7jbfQ6FlK5NDQ6v5MvP8cDUN28F8UX0rtw"
-      title="First Meetup"
-      address="Some Street 5, Seoul City"
-      description="The first meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 // getStaticPaths(): 동적 라우팅을 사용하는 페이지에서 정적 생성을 할 때 사용되는 함수. 빌드 시에 어떤 경로들이 미리 렌더링 되어야 하는지 결정하는데 사용
 export async function getStaticPaths() {
+  // MongoDB에 연결
+  const client = await MongoClient.connect(
+    'mongodb+srv://ldw0123:<password>@cluster0.fc5z6ma.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+  );
+
+  const db = client.db(); // meetups에 연결 중인 데이터베이스를 확보
+
+  const meetupsCollection = db.collection('meetups'); // collection: SQL DB에 있는, 여러 문서를 보관하고 있는 테이블
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     // fallback: false이면, paths에 정의되지 않은 모든 경로는 404 페이지로 이동. true이면, 사용자가 요청한 경로가 paths에 없을 경우, 사용자에게 빈 페이지를 보여줌
     // paths: 미리 렌더링할 경로들의 배열
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: 'm1',
-        },
-        params: {
-          meetupId: 'm2',
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
@@ -37,15 +44,30 @@ export async function getStaticProps(context) {
   // single meetup에 데이터를 가져오기(fetch)
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(
+    'mongodb+srv://ldw0123:<password>@cluster0.fc5z6ma.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  // findOne(): 하나의 문서를 찾는 메서드
+  // ObjectId(): MongoDB에서는 id값을 이상하고 긴 문자열로 저장하므로, 이를 문자열에서 객체로 변환해야 한다
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        image:
-          'https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRciG1cJNU2FQOPpOBcaAw9NCxixEPfW_YQSXHhfVOkitN0ZFQ1_Bf_vcWepNDleYklz0zA7jbfQ6FlK5NDQ6v5MvP8cDUN28F8UX0rtw',
-        id: meetupId,
-        title: 'First Meetup',
-        address: 'Some Street 5, Seoul City',
-        description: 'The first meetup',
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
