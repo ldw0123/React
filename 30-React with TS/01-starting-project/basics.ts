647593e86855be19ff9dce2507af1fdc800ca549
ldw0123 <ldw0123@gmail.com>
2024-07-10 20:48:53 +0900

    🎉 React with TypeScript

---
+++ 30-React with TS/01-starting-project/basics.ts
@@ -0,0 +1,13 @@
+// 주요 기본형 타입: number, string, boolean
+
+let age: number;
+age = 12.3;
+
+let userName: string;
+userName = 'Asahi';
+
+let isInstructor: boolean;
+isInstructor = false;
+
+let hobbies: null; // null 타입으로 지정하는 경우는 거의 없다
+// hobbies = 12; // 에러!
