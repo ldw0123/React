e240ed7d675e9fe910b1a7bfd058bef0fcf6fd99
ldw0123 <ldw0123@gmail.com>
2024-07-16 23:58:58 +0900

    ✨ props 전달하기

--- 04-React-DeepDive/01-starting-project/src/components/Section.jsx
+++ 04-React-DeepDive/01-starting-project/src/components/Section.jsx
@@ -1,6 +1,8 @@
-export default function Section({ title, id, children }) {
+// ... (rest 파라미터): 나머지 매개변수(여기서는 props)들을 모두 담는다
+export default function Section({ title, children, ...props }) {
   return (
-    <section id={id}>
+    // ... (spread 연산자): 배열이나 객체를 펼친다
+    <section {...props}>
       <h2>{title}</h2>
       {children}
     </section>
