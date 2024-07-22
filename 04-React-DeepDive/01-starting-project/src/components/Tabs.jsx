dca2e1f46d4a0db22e81e298da51022d9c716c6a
ldw0123 <ldw0123@gmail.com>
2024-07-22 22:07:59 +0900

    ✨ 속성 값 설정

--- 04-React-DeepDive/01-starting-project/src/components/Tabs.jsx
+++ 04-React-DeepDive/01-starting-project/src/components/Tabs.jsx
@@ -1,5 +1,5 @@
 // {children}: 컴포넌트의 자식 요소
-export default function Tabs({ children, buttons, buttonsContainer }) {
+export default function Tabs({ children, buttons, buttonsContainer = 'menu' }) {
   const ButtonContainer = buttonsContainer; // 커스텀 컴포넌트
   return (
     <>
