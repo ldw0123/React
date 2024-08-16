c7e2df56362a0898136a1d21f7de85e43ee97d8f
ldw0123 <ldw0123@gmail.com>
2024-08-16 11:51:39 +0900

    💡 주석 변경

--- 01-TypeScript/02_classEx.ts
+++ 01-TypeScript/02_classEx.ts
@@ -1,7 +1,7 @@
 // 단어사전 프로그램
 
 type Words = {
-  [key: string]: string; // 인덱스 시그니처(Index Signature). 객체의 키(key)를 어떤 타입으로, 그에 상응하는 값(value)을 어떤 타입으로 가지는지를 정의
+  [key: string]: string; // 인덱스 시그니처(Index Signature): 객체의 키(key)와 값(value)의 타입을 정의하는 것. [key: KeyType]: ValueType; 와 같은 형식
   // 위는, Words 타입이 string타입의 key, value를 property로 가지는 객체
 };
 
@@ -12,26 +12,24 @@ class Dict {
   constructor() {
     this.words = {};
   }
+  // 단어사전에 단어를 추가하는 메서드
+  // 클래스(Word)를 타입으로 사용! word 매개변수가 Word 클래스의 인스턴스로 사용된다
   add(word: Word) {
-    // 클래스(Word)를 타입으로 사용!
-    // 단어사전에 단어를 추가하는 메서드
-    if (this.words[word.term] === undefined)
-      // 주어진 단어가 아직 사전에 없으면
-      this.words[word.term] = word.def;
+    // this.words[word.term]: 대괄호 표기법. object[key]와 같은 형식. key를 이용하여 value에 접근한다. 동적으로 객체에 접근할 수 있다
+    // 주어진 단어가 아직 사전에 없으면
+    if (this.words[word.term] === undefined) this.words[word.term] = word.def;
   }
+  // term을 이용해 단어를 불러오기
   def(term: string) {
-    // term을 이용해 단어를 불러오기
     return this.words[term];
   }
+  // 단어사전을 수정하는 메서드
   update(word: Word) {
-    // 단어사전을 수정하는 메서드
     if (this.words[word.term] !== undefined) this.words[word.term] = word.def;
   }
+  // 단어를 삭제하는 메서드
   del(term: string) {
-    // 단어를 삭제하는 메서드
-    if (this.words[term] !== undefined) {
-      delete this.words[term]; // delete 연산자: 객체의 속성을 삭제
-    }
+    if (this.words[term] !== undefined) delete this.words[term]; // delete 연산자: 객체의 속성을 삭제
   }
 }
 
